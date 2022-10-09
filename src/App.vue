c2fbc04ce46ee119237cdb8d4c1cae574402dcd7
wjh <1091509167@qq.com>
2022-10-09 20:23:58 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+                    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
